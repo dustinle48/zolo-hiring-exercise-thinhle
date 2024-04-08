c62732c9e0aa98a5d9b93095af379a274c7497f5
dustinle48 <ducthinh481994@gmail.com>
2024-04-08 22:13:17 +0700

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import "@mantine/core/styles.css";
+
+import { MantineProvider, createTheme } from "@mantine/core";
 import type { AppProps } from "next/app";
 
+const theme = createTheme({});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
